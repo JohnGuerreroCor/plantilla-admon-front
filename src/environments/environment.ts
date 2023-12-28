fab1166d1765e5470d6c10615939cafadbc23014
JohnGuerreroCor <john.guerrero@usco.edu.co>
2023-12-28 11:31:17 -0500

    Plantilla funcional

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   //production: true,
   //DESCOMENTAR LOCAL
-  URL_BACKEND: 'http://localhost:8080/plantilla_admon',
+  URL_BACKEND: 'http://localhost:8080/plantilla-administrador-1.0',
   //DESCOMENTAR PRODUCCIÓN
   //URL_BACKEND: 'https://sanagustin.usco.edu.co/',
 };
